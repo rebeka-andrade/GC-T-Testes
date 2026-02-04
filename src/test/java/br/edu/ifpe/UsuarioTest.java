edc7e53a736559ea2ecab5e6c243cf1c18b5370b
Vilmar Nepomuceno <vilmarnepomuceno@recife.ifpe.edu.br>
2026-02-04 15:24:08 -0300

    Implemetação do teste.

--- src/test/java/br/edu/ifpe/UsuarioTest.java
+++ src/test/java/br/edu/ifpe/UsuarioTest.java
@@ -12,7 +12,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class UsuarioTeste {
+public class UsuarioTest {
 
 
     @Mock
