8c5ab9c0ceb7f88d1ea5f1056c5b3c20bd85762a
Clarice Queiroz <clariceq98@gmail.com>
2026-03-04 01:55:56 +0000

    TC_018 GREEN: teste consertado

--- src/main/java/br/edu/ifpe/Atendimento.java
+++ src/main/java/br/edu/ifpe/Atendimento.java
@@ -4,7 +4,34 @@ public class Atendimento {
 
     private Cliente cliente;
     private PlanoSaude plano;
+    private String tipo;
+    private String medico;
+    private String acompanhante;
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public String getAcompanhante() {
+        return acompanhante;
+    }
+
+    public void setAcompanhante(String acompanhante) {
+        this.acompanhante = acompanhante;
+    }
+    
     public Cliente getCliente() {
         return cliente;
     }
