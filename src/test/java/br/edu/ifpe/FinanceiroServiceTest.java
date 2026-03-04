ddebb2876dc2df8194749de95c24cac4c9542869
Clarice Queiroz <clariceq98@gmail.com>
2026-03-04 03:43:02 +0000

    TC_027 RED: teste com falha

---
+++ src/test/java/br/edu/ifpe/FinanceiroServiceTest.java
@@ -0,0 +1,28 @@
+package br.edu.ifpe;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FinanceiroServiceTest {
+
+    @Test
+    void deveCalcularTotalERealizarCobrancaAmbulatorial() {
+
+        FinanceiroService financeiro = new FinanceiroService();
+
+        double valorConsulta = 200.0;
+        double valorExames = 150.0;
+        double valorMedicamentos = 50.0;
+
+        double total = financeiro.calcularTotal(
+                valorConsulta,
+                valorExames,
+                valorMedicamentos
+        );
+
+        String resultado = financeiro.cobrarPlano(total);
+
+        assertEquals(400.0, total);
+        assertTrue(resultado.contains("Plano de saúde cobrado"));
+    }
+}
